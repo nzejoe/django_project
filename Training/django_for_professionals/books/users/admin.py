f92be0c1bcc479af7f9c9074a74cda1546ba96f7
nzejoe <nzejoeworld@gmail.com>
2020-09-13 08:05:39 +0100

    updated users admin and created users forms

--- Training/django_for_professionals/books/users/admin.py
+++ Training/django_for_professionals/books/users/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from . forms import CustomUserCreationForm, CustomUserChangeForm
+from django.contrib.auth import get_user_model
 
-# Register your models here.
+
+CustomUser = get_user_model()
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    list_display = ['username', 'email']
+    model = CustomUser
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
