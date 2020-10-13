9c0fca2c2e48dee347865ef7410e42722086258d
nzejoe <nzejoeworld@gmail.com>
2020-10-13 15:04:01 +0100

    done training chapter15 of django for professionals

--- training/django_for_professionals/books/orders/views.py
+++ training/django_for_professionals/books/orders/views.py
@@ -1,4 +1,5 @@
 import stripe
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import Permission
 from django.views.generic.base import TemplateView
 from django.conf import settings
@@ -8,13 +9,15 @@ from django.shortcuts import render
 stripe.api_key = settings.STRIPE_TEST_SECRET_KEY
 
 
-class OrdersPageView(TemplateView):
+class OrdersPageView(LoginRequiredMixin, TemplateView):
     template_name = 'orders/purchase.html'
+    login_url = 'account_login'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['stripe_key'] =settings.STRIPE_TEST_PUBLISHABLE_KEY
+        context['stripe_key'] = settings.STRIPE_TEST_PUBLISHABLE_KEY
         return context
+
 
 
 def charge(request):
@@ -24,7 +27,6 @@ def charge(request):
     user = request.user
     # Add user permission
     user.user_permissions.add(permission)
-
     if request.method == 'POST':
         charge = stripe.Charge.create(
             amount=3900,
